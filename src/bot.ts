0af3f3757fab42763d590f9b1066ff81ce27027e
George <george.valuiskyi@gmail.com>
2025-05-30 12:11:52 +0300

    feat: long polling -> webhooks
    Switch from long polling to a webhook connection by adding Express.js to
    the bot, due to incompatibility with long polling on Render. Add
    HOST_URL and PORT fields to the environment variable files.

--- src/bot.ts
+++ src/bot.ts
@@ -1,24 +1,42 @@
-import { Bot as GrammyBot } from "grammy";
+import { Bot as GrammyBot, webhookCallback } from "grammy";
 import { env } from "@/config/env";
 import type { BotModule, FrameworkBot } from "@/types/models";
 import { logger } from "@/logger";
+import express, { type Express } from "express";
+import crypto from "node:crypto";
+
+const SECRET_TOKEN = crypto.randomBytes(64).toString("hex");
 
 export class Bot {
     private readonly bot: FrameworkBot;
+    private readonly app: Express;
 
     constructor(...modules: BotModule[]) {
         this.bot = new GrammyBot(env.BOT_TOKEN);
         for (const module of modules) {
             module.apply(this.bot);
         }
+        this.app = express();
+        this.app.use(express.json());
+        this.app.use(
+            `/${env.BOT_TOKEN}`,
+            webhookCallback(this.bot, "express", {
+                secretToken: SECRET_TOKEN,
+            }),
+        );
     }
 
     init(): void {
-        try {
-            this.bot.start();
-            logger.info("Bot launched successfully.");
-        } catch (e) {
-            logger.error(e, "Failed to launch bot.");
-        }
+        this.bot.api
+            .setWebhook(`${env.HOST_URL}/${env.BOT_TOKEN}`, {
+                secret_token: SECRET_TOKEN,
+            })
+            .then(() =>
+                logger.info(`Bot successfully launched at: ${env.HOST_URL}`),
+            )
+            .catch((e) => logger.error(e, "Failed to launch bot."));
+        this.app.listen(env.PORT, () => {
+            logger.info(`Bot launched and listening on port ${env.PORT}.`);
+        });
     }
 }
--- src/config/env.ts
+++ src/config/env.ts
@@ -1,8 +1,10 @@
-import { cleanEnv, str } from "envalid";
+import { cleanEnv, num, str, url } from "envalid";
 import "dotenv/config";
 
 export const env = cleanEnv(process.env, {
     BOT_TOKEN: str(),
     ADMIN_IDS: str(),
     GROUP_ID: str(),
+    HOST_URL: url(),
+    PORT: num(),
 });
